1db61bd9f41dce00bf1fde06b3c6078fea88ebd6
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-10-16 17:27:04 +0530

    Update test_exceptional.py

--- test/test_exceptional.py
+++ test/test_exceptional.py
@@ -3,5 +3,5 @@ from test.TestUtils import TestUtils
 class ExceptionalTest(unittest.TestCase):
     def test_exceptional(self):
         test_obj = TestUtils()
-        test_obj.yakshaAssert("TestExceptional",True,"exceptional")
+        test_obj.yakshaAssert("TestExceptional",True,"boundary")
         print("TestExceptional = Passed")
